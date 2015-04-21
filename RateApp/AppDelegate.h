acfad6a4ea88953a7738874262dd251dbd769607
kolesnikovakate <kolesnikovakate@gmail.com>
2015-04-21 12:19:02 +0300

    Создан проект + gitignore

---
+++ RateApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RateApp
+//
+//  Created by Екатерина Колесникова on 21.04.15.
+//  Copyright (c) 2015 Kolesnikova Ekaterina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RateApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RateApp
+//
+//  Created by Екатерина Колесникова on 21.04.15.
+//  Copyright (c) 2015 Kolesnikova Ekaterina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
