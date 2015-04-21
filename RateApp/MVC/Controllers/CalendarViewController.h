ce79789e01b04a1d8fce81f94f17ceffdc060d00
kolesnikovakate <kolesnikovakate@gmail.com>
2015-04-21 15:27:12 +0300

    RAPickerTableView для вертикального календаря

---
+++ RateApp/MVC/Controllers/CalendarViewController.h
@@ -0,0 +1,15 @@
+//
+//  CalendarViewController.h
+//  RateApp
+//
+//  Created by Екатерина Колесникова on 21.04.15.
+//  Copyright (c) 2015 Kolesnikova Ekaterina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarViewController : UIViewController
+
+
+@end
+
---
+++ RateApp/MVC/Controllers/CalendarViewController.m
@@ -0,0 +1,27 @@
+//
+//  CalendarViewController.m
+//  RateApp
+//
+//  Created by Екатерина Колесникова on 21.04.15.
+//  Copyright (c) 2015 Kolesnikova Ekaterina. All rights reserved.
+//
+
+#import "CalendarViewController.h"
+
+@interface CalendarViewController ()
+
+@end
+
+@implementation CalendarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
